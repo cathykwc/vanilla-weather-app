c710f6be950b6d4674a68ae28052992512b30825
cathykwc <71102690+cathykwc@users.noreply.github.com>
2020-10-28 20:45:20 -0700

    Showing current temperature

---
+++ src/app.js
@@ -0,0 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement= document.querySelector("#humidity");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let windElement = document.querySelector("#wind");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+}
+
+let city = "New York";
+let apiKey = "145ee08408729673b53d671c97d81b6d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
